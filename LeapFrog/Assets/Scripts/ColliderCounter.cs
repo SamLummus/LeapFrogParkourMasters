a378d8c385b2bf6a3f1787d67fe0859495c8e3d4
SmilingWaffles <117694565+SmilingWaffles@users.noreply.github.com>
2024-02-25 15:23:41 -0700

    Added UI elements
    Fixed trigger issues as well for how the game counts that stuff

--- LeapFrog/Assets/Scripts/ColliderCounter.cs
+++ LeapFrog/Assets/Scripts/ColliderCounter.cs
@@ -5,34 +5,43 @@ using TMPro;
 
 public class ColliderCounter : MonoBehaviour
 {
+    public enum PlayerType { Player1, Player2 };
+    public PlayerType playerType;
+
     CapsuleCollider capsuleCollider;
-    [SerializeField] private TextMeshProUGUI playerPointUI;
-    int points = 0;
-    void Start()
-    {
-        capsuleCollider = GetComponent<CapsuleCollider>();
-        gameObject.tag = "Player1";
-    }
+    [SerializeField] private TextMeshProUGUI player1PointUI, player2PointUI, winTextUI;
 
-    // Update is called once per frame
-    void Update()
-    {
+    int player1Points, player2Points = 0;
 
+    private void Start() {
+        player1PointUI.text = "Player 1 score: " + player1Points.ToString();
+        player2PointUI.text = "Player 2 score: " + player2Points.ToString();
     }
 
     private void OnTriggerEnter(Collider other) {
-
-        if (other.gameObject.tag == "Player2") {
-
-            Debug.Log("Triggered by Player 2");
+        if (other.gameObject.tag == "Player") {
+            IncreaseScore();
         }
     }
-    void OnTriggerExit(Collider other) {
 
-        if (other.gameObject.tag == "Player2") {
+    private void IncreaseScore() {
+        if (playerType == PlayerType.Player1) {
+            player2Points++;
+            player2PointUI.text = "Player 2 score: " + player2Points.ToString();
 
-            playerPointUI.text = "Player 1: " + points++;
+            if (player2Points > 3) {
+
+                winTextUI.text = "Player 2 Wins!";
+            }
+        } else {
+            player1Points++;
+            player1PointUI.text = "Player 1 score: " + player1Points.ToString();
+
+            if (player1Points > 3) {
+
+                winTextUI.text = "Player 1 Wins!";
+            }
         }
-        
     }
+
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,9 @@
 For this game of Leap Frog Parkour Masters, I added two platforms at either end of the arena, with one tower in the middle with the biggest obstacle of all, the Spin Log.
 The spin log is a deterrant to other players who want to rush their opponent off the bat. The log can be bounced on top of, and if used skillfully, the player bouncing and use it to rack up points against the opponent. The game has potential to be more fun, but these were the added game design tools to improve the game.
+
+February 25, 2024
+The changes I made to the game were substantial to the game and fixed many issues
+- Fixed the detection of triggers by creating new boxes that hold the triggers and not the actual player colliders.
+- Fixed the way the game counts scores so that way the scores don't get counted in twos or a million times.
+- Added better UI that reflects the game state and such.
+- The points being shown accurately reflect who is scoring the point, no longer will player one score for player 2.
